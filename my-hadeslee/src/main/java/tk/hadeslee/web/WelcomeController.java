60b92bd233d1b92fc8d8efe8e7f578403e82793c
sklee <hadesleeq@gmail.com>
2017-03-15 17:45:16 +0900

    welcome controllor 추가

--- my-hadeslee/src/main/java/tk/hadeslee/web/WelcomeController.java
+++ my-hadeslee/src/main/java/tk/hadeslee/web/WelcomeController.java
@@ -1,6 +1,7 @@
 package tk.hadeslee.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -15,7 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
     @GetMapping("/helloworld")
-    public String welcome() {
+    public String welcome(String name, int age, Model model) {
+        System.out.println("name : " + name + ", age :" + age);
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
         return "welcome";
     }
 }
