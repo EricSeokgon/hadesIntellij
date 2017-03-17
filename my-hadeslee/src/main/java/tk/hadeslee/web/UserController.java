b7fc2eccf13014018bbb447ec7c3763ccb338de5
sklee <hadesleeq@gmail.com>
2017-03-17 16:08:44 +0900

    회원가입 기능 구현

--- my-hadeslee/src/main/java/tk/hadeslee/web/UserController.java
+++ my-hadeslee/src/main/java/tk/hadeslee/web/UserController.java
@@ -1,7 +1,12 @@
 package tk.hadeslee.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Project: hadesIntellij
@@ -14,9 +19,20 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class UserController {
+    private List<User> users = new ArrayList<>();
+
     @PostMapping("/create")
     public String create(User user) {
         System.out.println("User : " + user);
+        users.add(user);
         return "index";
     }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
+    }
+
 }
+
