e628dbcc2e231673e35e237f2888fff2f4ff4497
sklee <hadesleeq@gmail.com>
2017-03-17 15:27:09 +0900

    회원가입 기능 구현

--- my-hadeslee/src/main/java/tk/hadeslee/web/UserController.java
+++ my-hadeslee/src/main/java/tk/hadeslee/web/UserController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
     @PostMapping("/create")
-    public String create(String userId, String password, String name, String email) {
-        System.out.println("userID : " + userId + " name : " + name);
+    public String create(User user) {
+        System.out.println("User : " + user);
         return "index";
     }
 }
