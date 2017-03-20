8f9c7244c3754880625d22eec9725074e0bc0cef
sklee <hadesleeq@gmail.com>
2017-03-20 18:01:15 +0900

    새로운 html 적용 및 url 리팩토링

--- my-hadeslee/src/main/java/tk/hadeslee/web/UserController.java
+++ my-hadeslee/src/main/java/tk/hadeslee/web/UserController.java
@@ -5,11 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import tk.hadeslee.domain.User;
 import tk.hadeslee.domain.UserRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Project: hadesIntellij
@@ -21,24 +19,36 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private List<User> users = new ArrayList<>();
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/create")
+    @GetMapping("/loginForm")
+    public  String loginForm() {
+        return "/user/login";
+    }
+
+    @GetMapping("/form")
+    public  String form() {
+        return "/user/form";
+    }
+
+    @PostMapping("")
     public String create(User user) {
         System.out.println("User : " + user);
         userRepository.save(user);
-        return "redirect:/list";
+        return "redirect:/users";
     }
 
-    @GetMapping("/list")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        return "list";
+        return "/users/list";
     }
+
+
 
 }
 
